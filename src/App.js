a3c3a662dfb6238235c83f7426ba136c79aa92f8
Enkhtushig Namkhai <enkhtushig@Enkhtushigs-MacBook-Pro.local>
2023-05-09 14:45:16 -0700

    docker course, initial commit for containerized code

--- src/App.js
+++ src/App.js
@@ -6,8 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p> 
+          Hi there. I can make a hot change now!! 
+          Hot reloading: Whenever you make a change in the file, the server detects it and automatically refreshes the view in the browser.
+          <br></br>
+          Versus normal saves do not automatically update the view in the browser! You have to save, and then manually refresh the page to see the changes.
+          <br></br>
+          React server offers this hot reload functionality. 
+          Note that react server that we are running to serve the app, supports hot reloading, but it needs to detect the changes in the files itself! 
+          When we use Docker containers, we copy over our src files. But for react server to detect the changes, we instead make a reference to our src files for the docker container to use. So, the React server that is running inside the container now sees the change and it updates the page automatically with hot reload! 
+          <br></br>
+          Docker volume lets us map the container's files to our hosts files.
+          So docker volume only references the file at runtime! The image that you use will still have the old copy of the file system (the last time you built the image).
+          To push the changes to the image itself, you still have to build to create a new image/edit the image? (not sure which happens)
         </p>
         <a
           className="App-link"
